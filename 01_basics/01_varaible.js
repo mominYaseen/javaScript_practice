7c2f095b5c7995565817673f61ff5eb932fea55c
mominYaseen <142157323+mominYaseen@users.noreply.github.com>
2024-09-24 09:22:24 +0000

    container config

---
+++ 01_basics/01_varaible.js
@@ -0,0 +1,12 @@
+const accountId = 12345
+let accountEmail = "m@gmail.com"
+var accountPassword = "77777"
+accountCity = "Kashmir"
+let accountState;
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+accountEmail = " y@gmail.com"
+accountPassword = "56656"
+accountCity = "banglore"
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
